3cf0e950a16cc18daf98797f84c8091ca01e61dc
Trentyn <trentynpowell@gmail.com>
2022-03-19 11:30:14 -0700

    vars

--- pseudo_code_format.js
+++ pseudo_code_format.js
@@ -1,1 +1,17 @@
 ../pseudo_code_format.js
+
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+var timeEl = document.querySelector("#time");
+var startBtn = document.querySelector("#startButton");
+var submitBtn = document.querySelector("#submit-button");
+var titleScreen = document.querySelector("#title-section");
+var quizScreen = document.querySelector("#quiz-section");
+var highScoreScreen = document.querySelector("#highscore-section");
+var highScoreDisplay = document.querySelector("#highscore-display-section");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+
+var questionsEl = document.querySelector("#question");
+var choicesEl = document.querySelector("#choices");
